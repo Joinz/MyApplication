ad66e755da23d58a22efb4a6857e4e07f7a7d7d4
Joinz <joinz@mail.ru>
2018-11-06 12:33:43 +0200

    Initial commit

---
+++ app/src/main/java/myapplicationn/joinz/com/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package myapplicationn.joinz.com.myapplication;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public String name;
+    private TextView label;
+    private int counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("MainActivity", "onCreate called");
+
+        label = findViewById(R.id.label);
+        counter++;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", counter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt("counter", counter);
+    }
+}
